295db7379afe1224db3403daf8c136e7440422e0
Quipildor-Claudio <claudioq00@gmail.com>
2024-01-22 01:37:24 -0300

    implents nodemon and static public

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
+let path = require('node:path');
 
-app.get('/cart', (req, res) => {
-    res.send('Hola, estamos en ');
+app.get('/', (req, res) => {
+    res.sendFile(path.join( __dirname,'/views/home.html'));
 });
-
-app.listen(3005,()=>console.log('Server up to port 300'));
+app.use(express.static('public'))
+app.listen(3005,()=>console.log('Server up to http://localhost:3005 '));
